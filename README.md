70edcb71a6da6e331ef9bc7de0fcc401d00e219e
sachinrao24 <sachinkrao7@gmail.com>
2025-01-23 00:39:51 +0530

    revamped website

---
+++ README.md
@@ -0,0 +1,4 @@
+# portfolio
+Curated research portfolio website template
+
+https://raj-1411.github.io/portfolio/
---
+++ assets/js/main.js
@@ -0,0 +1,252 @@
+$(document).ready(function(){
+
+	// About Me content is the one by default shown
+	$('#publicationsContent').hide();
+	$('#educationContent').hide();
+	$('#interestsContent').hide();
+
+	$('#theme').hide();
+
+	$.getJSON("https://api.countapi.xyz/hit/nicolasmeseguer.github.io/634c2142-b35d-430e-b51c-dad16880dd3a", function(response) {
+		$("#contadorVisitas").text(response.value);
+	});
+
+	// First time, check the theme
+	if(localStorage.getItem("theme") === null){
+		localStorage.theme = "light";
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+			localStorage.theme = "dark";
+	}
+
+	// Maybe first time or not, so load the localStorage value
+	$('<link>').appendTo('head').attr({
+		type: 'text/css', 
+		rel: 'stylesheet',
+		href: 'assets/css/light.css'
+	});
+	if (localStorage.theme == "dark") {
+		// Handle menu
+		$("link[href='assets/css/light.css']").remove();
+		$('<link>').appendTo('head').attr({
+			type: 'text/css', 
+			rel: 'stylesheet',
+			href: 'assets/css/dark.css'
+		});
+		$('#theme').empty().append("<i class='fa-duotone fa-lightbulb-slash'></i>");
+	}
+	// Done because light is the one by default
+
+	// Handle 'About Me' content
+	$('#aboutme').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#aboutmeContent');
+		}
+
+	});
+
+	// Handle 'Education' content
+	$('#education').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#educationContent');
+		}
+	});
+
+	// Handle 'Publications' content
+	$('#publications').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#publicationsContent');
+		}
+	});
+
+	// Handle 'Interests' content
+	$('#interests').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#interestsContent');
+		}
+	});
+
+	/*
+	// Handle 'Template' content
+	$('#name').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#nameContent');
+		}
+	});
+	*/
+
+	// Whenever you clic on a blog post, you should be redirected to that post' html
+	$('.clickable').click(function(e) {
+		window.open($(e.currentTarget)[0].childNodes[1].innerText, '_blank').focus();
+	});
+
+	// Whenever you clic on a clases particulares heading, you should toggle
+	$('.particular-clickable').click(function(e) {
+		var e = $(e.currentTarget)[0].nextElementSibling;
+		if (e.style.display === "none") {
+			e.style.display = "block";
+		}
+		else {
+			e.style.display = "none";
+		}
+	});
+
+	// Copy the citation to the clipboard
+	// THIS SHOULD BE THE SAME FOR ALL THE PAPERS
+	$(document).on("click", "#citation", function(){
+		var text = $(this).parent().parent().next()[0].innerHTML;
+
+		navigator.clipboard.writeText(text);
+
+		toastr.success('Citation copied');
+	});
+
+	// Controls the URL; if it has '#blog'
+	// then trigger the 'Blog' clic
+	// if (((window.location).href).substring(((window.location).href).lastIndexOf('#') + 1) == 'tutorials') {
+	// 	$('#tutorials').click();
+	// 	$('#tutorialsContent').focus();
+	// }
+
+	if (((window.location).href).substring(((window.location).href).lastIndexOf('#') + 1) == 'clases-particulares') {
+		$('#particular').click();
+		$('#particularContent').focus();
+	}
+
+	// Controls the options menu
+	$('#options-toggler').click(function(e) {
+		if(!$(e.currentTarget).hasClass('active')) {
+			$(e.currentTarget).addClass('active');
+			$('#theme').show("fast");
+		}
+		else {
+			$(e.currentTarget).removeClass('active');
+			$('#theme').hide("fast");
+		}
+	})
+
+	// Animates the theme button + functionality
+	$('#theme').click(function(e) {
+		if(localStorage.theme != "dark"){
+
+			$('#theme').empty().append("<i class='fa-duotone fa-lightbulb-slash'></i>");
+
+			localStorage.theme = "dark"
+			
+			$("link[href='assets/css/light.css']").remove();
+			$('<link>').appendTo('head').attr({
+				type: 'text/css', 
+				rel: 'stylesheet',
+				href: 'assets/css/dark.css'
+			});
+		}
+		else {
+
+			$('#theme').empty().append("<i class='fa-duotone fa-lightbulb'></i>");
+
+			localStorage.theme = "light"
+			
+			$("link[href='assets/css/dark.css']").remove();
+			$('<link>').appendTo('head').attr({
+				type: 'text/css', 
+				rel: 'stylesheet',
+				href: 'assets/css/light.css'
+			});
+		}
+	})
+
+});
+
+function clearActiveLinks() {
+	$('#navbarList .nav-item .nav-link').each(function() {
+		$(this).removeClass('active');
+	});
+}
+
+function clearActiveDivs() {
+	$('.container .content .active').each(function() {
+		$(this).removeClass('active');
+		$(this).hide();
+	});
+}
+
+function activateLink(e) {
+	$(e.target).addClass('active');
+	
+	// Hide left panel
+	if(e.target.id == "particular")
+		$('#leftPanel').hide();
+	else
+		$('#leftPanel').show();
+}
+
+function activateDiv(divId) {
+	$(divId).addClass('active');
+	$(divId).show();
+
+	// Scrolls to the content
+	scrollToContent(divId);
+}
+
+function scrollToContent(divId) {
+	if ($(window).width() < 751) {
+		$('html, body').animate({
+			scrollTop: $(divId).offset().top
+		}, 1);
+	}
+}
+
+function resetViews() {
+	$.getJSON("https://api.countapi.xyz/set/nicolasmeseguer.github.io/634c2142-b35d-430e-b51c-dad16880dd3a?value=0", function(response) {
+		$("#contadorVisitas").text("0");
+	});
+}
